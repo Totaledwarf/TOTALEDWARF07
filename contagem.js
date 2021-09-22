bbac2a45866ee088d996a6672f75352b79536a60
Thiago Souza <90399893+Totaledwarf@users.noreply.github.com>
2021-09-21 21:53:33 -0300

    Add files via upload

---
+++ contagem.js
@@ -0,0 +1,13 @@
+var QuantidadeDeAluno = 14;
+
+for (var contador = 0; contador < QuantidadeDeAluno; contador++){
+    if (contador == 0{
+        console.log("o numero é zero");
+    }
+    else if(contador % 2 == 0){
+        console.log(contador + "O numero é par");
+    }
+    else{
+        console.log(contador + "O numero é impar")
+    }
+}
