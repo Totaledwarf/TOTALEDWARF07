261153de0db3429f7807a37e03ce24b68aad364c
Thiago Souza <90399893+Totaledwarf@users.noreply.github.com>
2021-12-07 18:52:39 -0300

    Add files via upload

---
+++ codificacao.js
@@ -0,0 +1,9 @@
+           var senha = 8 ;
+
+
+           if ( senha < 8 ) {
+               console . log ( "senha invalida" ) ;
+           }
+           else {
+               console . log ( "senha valida" )
+           }
