c62af96383b6e459d52cf2d090db4985651a8100
Thiago Souza <90399893+Totaledwarf@users.noreply.github.com>
2021-12-07 18:38:43 -0300

    Add files via upload

--- contagem.js
+++ contagem.js
@@ -1,13 +1,12 @@
-var QuantidadeDeAluno = 14;
+var quantidadedealunos = 14 ;
 
-for (var contador = 0; contador < QuantidadeDeAluno; contador++){
-    if (contador == 0{
-        console.log("o numero é zero");
+para ( var contador = 0 ; contador < quantidadedealunos ; contador ++ ) {
+    if ( contador == 0 ) {
+        console . log ("o numero é zero ")
     }
-    else if(contador % 2 == 0){
-        console.log(contador + "O numero é par");
-    }
-    else{
-        console.log(contador + "O numero é impar")
-    }
+else if ( contador % 2 == 0 ) {
+    console . log ( contador + "o numero é par" ) ;
+}
+else {
+    console . log ( contador + "o numero e impar" )
 }
